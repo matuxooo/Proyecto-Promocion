535a6fe9e1832bd6dd689231b96336a49f8ea334
Juan_Ajmat <juan.ajmat@gmail.com>
2023-03-24 18:23:36 -0300

    agregado conex

--- Conexion.vhd
+++ Conexion.vhd
@@ -1,4 +1,13 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use work.ffd_pkg.all;
+use IEEE.numeric_std.all;
+
+
+
+architecture solucion of conex is
 begin
+
     sincro: sync
     port map(
         rst     =>  rst    ,
@@ -8,4 +17,38 @@ begin
         muestra =>  muestra,
         fila    =>  fila   ,
         columna =>  columna
-    )
+    );
+
+    gen: generador
+    port map(
+        fila_z    =>fila_z    ,
+        columna_z =>columna_z ,
+        char      =>char      ,
+        muestra   =>muestra   ,
+        px_visible=>px_visible
+    );
+
+    pos_txt: posicion_texto
+    port map(
+        fila       =>fila       ,
+        columna    =>columna    ,
+        muestra    =>muestra    ,
+        fila_z     =>fila_z     ,
+        columna_Z  =>columna_Z  ,
+        n_zona     =>n_zona     ,
+        codigo_char=>codigo_char,
+        char       =>char       ,
+        valido     =>valido     
+    );
+
+    tabla_char: tabla_caract
+    port map(
+        codigo_char => codigo_char,
+        char        => char       
+    );
+
+    
+
+
+
+end;
